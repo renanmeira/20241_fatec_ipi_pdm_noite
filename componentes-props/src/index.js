94a2721e801bdb6332ada38e4596f23c0820a90f
Renan Meira <renan.meira@gmail.com>
2024-03-01 22:21:09 -0300

    feat(ap04): fim secao 2.5

--- componentes-props/src/index.js
+++ componentes-props/src/index.js
@@ -11,22 +11,64 @@ const App = () => {
         </div>
         <div className="row">
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="bg-secondary">Caixa</div>
+                <div className="card">
+                    <div className="card card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-regular fa-hard-drive fa-2x fa-beat-fade"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-1 border">
+                            <h4 className="text-center">HD</h4>
+                            <p className="text-center">HD 1TB</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-               <div className="bg-secondary">Caixa</div>
+                <div className="card">
+                    <div className="card card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-solid fa-memory fa-2x fa-beat-fade"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-1 border">
+                            <h4 className="text-center">RAM</h4>
+                            <p className="text-center">Memória 16GB</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-               <div className="bg-secondary">Caixa</div>
-            </div>
+                <div className="card">
+                    <div className="card card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                        <i className="fa-solid fa-vial-circle-check fa-2x fa-beat-fade"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-1 border">
+                            <h4 className="text-center">Injeção</h4>
+                            <p className="text-center">SQL injection</p>
+                        </div>
+                    </div>
+                </div>            </div>
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="bg-secondary">Caixa</div>
-            </div>
+                <div className="card">
+                    <div className="card card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-solid fa-terminal fa-2x fa-beat-fade"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-1 border">
+                            <h4 className="text-center">Terminal</h4>
+                            <p className="text-center">Terminal</p>
+                        </div>
+                    </div>
+                </div>            </div>
         </div>
     </div>
 }
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.querySelector('#root')
 )
